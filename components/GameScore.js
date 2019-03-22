57506c9cd2e46272113a71eb8bd75b981e713e80
Chintan Shah <chintans98@gmail.com>
2019-03-22 12:03:23 -0400

    First revision of game score design
    - still need to clean up styles and use the styles/ folder
    - still need more information about the game (score or start time or end game score)

--- components/GameScore.js
+++ components/GameScore.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 import { colors } from '../styles'
 
@@ -9,9 +9,21 @@ export default class GameScore extends Component {
 
     return (
       <View style={styles.card}>
-        <Text style={styles.itemText}>
-          {homeTeam} vs {roadTeam}
-        </Text>
+        <View style={styles.team}>
+          <View style={styles.teamLogoContainer}>
+            <Image style={styles.teamLogo} />
+          </View>
+          <Text style={styles.teamText}>{homeTeam}</Text>
+        </View>
+
+        <Text style={styles.middleText}>vs</Text>
+
+        <View style={styles.team}>
+          <View style={styles.teamLogoContainer}>
+            <Image style={styles.teamLogo} />
+          </View>
+          <Text style={styles.teamText}>{roadTeam}</Text>
+        </View>
       </View>
     );
   }
@@ -19,15 +31,46 @@ export default class GameScore extends Component {
 
 const styles = StyleSheet.create({
   card: {
+    alignItems: 'center',
     backgroundColor: colors.secondaryColor,
+    borderRadius: 5,
+    flex: 1,
+    flexDirection: 'row',
     margin: 10,
-    paddingVertical: 15,
+    padding: 10
+  },
+
+  team: {
+    alignItems: 'center',
+    flex: 1,
+    flexDirection: 'column'
+  },
+
+  teamLogoContainer: {
     alignItems: 'center'
   },
 
-  itemText: {
+  teamLogo: {
+    backgroundColor: colors.backgroundColor,
+    borderRadius: 60 / 2,
+    width: 60,
+    height: 60,
+  },
+
+  teamText: {
     color: colors.primaryTextColor,
-    fontSize: 20,
-    fontWeight: 'bold'
+    flex: 2,
+    fontSize: 14,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    paddingTop: 10
+  },
+
+  middleText: {
+    color: colors.secondaryTextColor,
+    flex: 1,
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center'
   }
 })
--- styles/colors.js
+++ styles/colors.js
@@ -1,9 +1,11 @@
 const backgroundColor = "#2c3e50";
 const primaryTextColor = "#ecf0f1";
+const secondaryTextColor = "#95a5a6";
 const secondaryColor = "#34495e";
 
 export {
   backgroundColor,
   primaryTextColor,
+  secondaryTextColor,
   secondaryColor
 };
