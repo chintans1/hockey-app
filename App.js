95d3915d55318f57c4580599b08995c6d63f8445
Chintan Shah <chintans98@gmail.com>
2019-03-21 21:09:36 -0400

    Move back App.js to root folder, add react-navigation, etc

---
+++ App.js
@@ -0,0 +1,31 @@
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+
+import HomeScreen from './src/screens/HomeScreen';
+import GameScreen from './src/screens/GameScreen';
+
+const headerStyles = {
+  headerBackTitle: null,
+  headerTintColor: '#2c3e50',
+  headerStyle: {
+    borderBottomWidth: 0,
+    backgroundColor: '#ecf0f1'
+  },
+  headerTitleStyle: {
+    fontWeight: 'bold'
+  }
+}
+
+const AppNavigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Game: GameScreen
+  },
+  {
+    initialRouteName: 'Home',
+    defaultNavigationOptions: {
+      ...headerStyles
+    }
+  }
+);
+
+export default createAppContainer(AppNavigator);
